70d8a20c7cb6ba723a50b6600c48d192339e0313
anyamiyeth <anyamiyeth@byasystems.com.co>
2021-11-17 22:18:17 -0500

    Agregar archivos de proyecto.

---
+++ CronJob/Base/Cron/IScheduleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CronJob.Base.Cron
+{
+    public interface IScheduleConfig<T>
+    {
+        string CronExpression { get; set; }
+        TimeZoneInfo TimeZoneInfo { get; set; }
+    }
+}
---
+++ CronJob/Services/MyCronJob.cs
@@ -0,0 +1,39 @@
+﻿using CronJob.Base.Cron;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CronJob.Services
+{
+    public class MyCronJob : CronJobService
+    {
+        private readonly ILogger<MyCronJob> _logger;
+
+        public MyCronJob(IScheduleConfig<MyCronJob> config, ILogger<MyCronJob> logger)
+            : base(config.CronExpression, config.TimeZoneInfo)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CronJob starts.");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task DoWork(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{DateTime.Now:hh:mm:ss} CronJob is working.");
+            return Task.CompletedTask;
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CronJob is stopping.");
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
